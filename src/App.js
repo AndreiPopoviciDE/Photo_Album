f04df8ec5732ec3881af4057f47db7f202e46f8a
Andrei-PC <andrei.popovici.de@gmail.com>
2021-09-23 14:10:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import React, {useState} from 'react';
+import Navigation from './components/Navigation';
+import Users from './components/Users';
+import Albums from './components/Albums';
+import Home from './components/Home';
+import {getUsers, getCurrentUserId, setCurrentUserLocalStorage, setUsersToLocalStorage, login, resetCurrentUser} from './data/usersData';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import UserProfile from './components/UserProfile';
+import { getAlbums, setAlbumsToLocalStorage } from './data/albumsData';
+import { getPhotos, setPhotosToLocalStorage } from './data/photosData';
+import AlbumPhotos from './components/AlbumPhotos';
+
+export const AppContext = React.createContext()
 
 function App() {
+  const [users, setUsers] = useState(getUsers());
+  const [currentUser, setCurrentUser] = useState(getCurrentUserId());
+  //const [error, setError] = useState (null)
+
+  const addUser = (user)=>{
+    const isUserExist = users.some(u => u.email === user.email)
+    if(!isUserExist){
+    const newUser = {...user, id: Date.now()}
+    const newUsersArray = [...users, newUser]
+    setUsers(newUsersArray)
+    setUsersToLocalStorage(newUsersArray)
+    setCurrentUser(newUser.id)
+    setCurrentUserLocalStorage(newUser.id)
+    return true
+    }
+    return false
+  }
+
+  const changeCurrentUser = (currentUser) =>{
+    const idUser = login(currentUser)
+    if(idUser){
+      setCurrentUser(idUser);
+      setCurrentUserLocalStorage(idUser)
+      return true
+    }
+      return false
+  }
+
+  const getCurrentUser = ()=>{
+    return users.find(user => user.id === currentUser)
+  }
+
+  const getUserNameById = (id)=>{
+    return users.find(user => +user.id === id).fName
+  }
+
+  const logout = ()=>{
+    setCurrentUser(null)
+    resetCurrentUser()
+  }
+
+  const updateUser = user =>{
+    const newUsers = [...users]
+    const index = users.indexOf(getCurrentUser()) // users.findIndex(u => u.id === user.id)
+    newUsers[index] = user
+    setUsers(newUsers)
+    setUsersToLocalStorage(newUsers)
+  }
+
+  const [albums, setAlbums] = useState(getAlbums());
+
+  const addNewAlbum = album =>{
+    const newAlbums = [...albums, {...album, id:Date.now()}]
+    setAlbums(newAlbums)
+    setAlbumsToLocalStorage(newAlbums)
+  }
+
+  const currentUserAlbums = ()=>{
+    return albums.filter(album => album.userId === currentUser)
+  }
+
+  const getAlbumTitleById =(id) =>{
+    return albums.find(album => album.id === +id).title
+  }
+
+  const [photos, setPhotos] = useState(getPhotos())
+
+  const addNewPhoto = photo =>{
+    const newPhotos = [...photos, {...photo, id:Date.now(), like:0, dislike:0}]
+    setPhotos(newPhotos)
+    setPhotosToLocalStorage(newPhotos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value = {{
+      users,
+      addUser,
+      changeCurrentUser,
+      currentUser,
+      getUserNameById,
+      logout,
+      getCurrentUser,
+      updateUser,
+      addNewAlbum,
+      currentUserAlbums,
+      addNewPhoto,
+      photos,
+      albums,
+      getAlbumTitleById
+
+    }} >
+      <Navigation />
+      <Switch>
+        <Route exact path = "/album/:id/:author" component = {AlbumPhotos} />
+        <Route exact path = "/user/:id" component = {UserProfile}/>     
+        <Route path ="/users" component ={Users} /> 
+        <Route path ="/albums" component ={Albums} />
+        <Route path ="/login" component = {Login} />
+        <Route path = "/registration" component = {Registration} />
+        <Route path ="/" component ={Home} />
+      </Switch>
+    </AppContext.Provider>
   );
 }
 
